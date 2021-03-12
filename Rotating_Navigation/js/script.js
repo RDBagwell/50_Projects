b19554acab763551475ac5e6553be81b7c5a9be0
Robert B <robertb@lifesciencepublishers.com>
2021-03-12 12:13:41 -0700

    Rotating nav

--- Rotating_Navigation/js/script.js
+++ Rotating_Navigation/js/script.js
@@ -2,8 +2,6 @@ const openBTN = document.getElementById('open');
 const closeBTN = document.getElementById('close');
 const container = document.querySelector('.container');
 
-console.log('container')
-
 openBTN.addEventListener('click', ()=>{
     container.classList.add('show-nav');
 })
